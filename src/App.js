1330525becd06ac27482ec01739490ac34c5bd3a
Oleksandr Dobrozhan <dobrozhan.a@gmail.com>
2021-02-15 14:32:57 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import { createStore } from 'redux';
+import reducer from './reducer';
+import { Provider } from "react-redux";
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer />
+    </Provider>
+  );
+}
+
+export default App;
